fc1e13f02de3611ed8968415a37396705cb5fc8c
R-Mehdi94 <mehdi.bellilpro@gmail.com>
2022-09-27 11:41:46 +0200

    projet GSB-Frais

--- GSB-Frais-Appli-PDO-Etudiant/public/index.php
+++ GSB-Frais-Appli-PDO-Etudiant/public/index.php
@@ -7,3 +7,12 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+$url = '';
+if(isset($_GET['url'])){
+    $url = explode('/', $_GET['url']);
+}
+
+if($url== ''){
+        require '/GSB-Frais-Appli-PDO-Etudiant/templates/accueil/index.html';
+    }
